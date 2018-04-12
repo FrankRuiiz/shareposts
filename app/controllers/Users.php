8c71edd62500746748bbd9feaf1feb3d170eca4e
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-04-12 14:55:08 -0700

    add user registration initial validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -8,6 +8,21 @@
             // Check for post
             if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
                 // process form
+
+                // Sanitize POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                // Init data
+                $data = [
+                    'name' => trim($_POST['name']),
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'confirm_password' => trim($_POST['confirm_password']),
+                    'name_err' => '',
+                    'email_err' => '',
+                    'password_err' => '',
+                    'confirm_password_err' => ''
+                ];
             } else  {
                 // Load the form
                 
@@ -22,6 +37,23 @@
                     'password_err' => '',
                     'confirm_password_err' => ''
                 ];
+
+                // Validate Name
+                if (empty($data['name'])) {
+                    $data['name_err'] = 'Plese enter name';
+                }
+                // Validate Email
+                if (empty($data['email'])) {
+                    $data['email_err'] = 'Plese enter email';
+                }
+                // Validate Password
+                if (empty($data['password'])) {
+                    $data['password_err'] = 'Plese enter valid password';
+                }
+                // Validate Confirm Password
+                if (empty($data['confirm_password'])) {
+                    $data['password_confirmation_err'] = 'Plese enter password confirmation';
+                }
 
                 // Load view
                 $this->view('users/register', $data);
